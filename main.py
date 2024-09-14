0e49a341eaeeb40189e2c10e63aff086c84912e3
harry-lons <harrylonsdale25@gmail.com>
2024-09-13 23:33:17 -0700

    initial commit

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ from flask import Flask, request, jsonify
 from dotenv import load_dotenv
 from stravalib import Client
 import os
+import ssl
 from flask_cors import CORS
 
 load_dotenv()
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": os.getenv('FRONTEND_URL')}})
 
-
 @app.route("/")
 def home():
-    return "Home string"
+    return "Hello HTTPS!"
 
 @app.route("/get-token", methods=["POST"])
 def getToken():
     data = request.get_json()
-    code = data['code'] # e.g.
+    code = data['code']
     client = Client()
     try:
         token_response = client.exchange_code_for_token(client_id=os.getenv('CLIENT_ID'),
-                                                    client_secret=os.getenv('CLIENT_SECRET'),
-                                                    code=code)
-    except:
-        return {'type':'ERROR'}, 500
+                                                        client_secret=os.getenv('CLIENT_SECRET'),
+                                                        code=code)
+    except Exception as e:
+        return {'type':'ERROR', 'message': str(e)}, 500
     
     tokens = {
         'type': 'SUCCESS',
@@ -34,4 +34,8 @@ def getToken():
     return jsonify(tokens), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, port=3011)
+    if os.getenv('SSL_CRT') and os.getenv('SSL_KEY'):
+        context = ('localhost.crt', 'localhost.key')
+        app.run(ssl_context=context, debug=True, port=3011)
+    else:
+        app.run(debug=True, port=3011)

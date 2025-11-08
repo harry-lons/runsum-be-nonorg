4fcea69dfae4a433ea75c8200f4844e9ff48582a
harry-lons <harrylonsdale25@gmail.com>
2025-11-07 19:35:41 -0800

    framework for the getActivities function

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, make_response, g
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, set_access_cookies
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity, set_access_cookies, unset_jwt_cookies
 from dotenv import load_dotenv
 from stravalib import Client
 from flask_cors import CORS
@@ -53,24 +53,18 @@ def authenticate_me():
         athlete = temp_client.get_athlete()
 
         athlete = temp_client.get_athlete() # Get user's full athlete record
-        print("Hello, {}. I know your id is {}".format(athlete.firstname, athlete.id))
-
+        # print(athlete)
+        athleteIdentity = {
+            "id": athlete.id,
+            "first_name": athlete.firstname
+        }
         # Create JWT token
-        JWT = create_access_token(identity=athlete.id, expires_delta=timedelta(days=30))
+        JWT = create_access_token(identity=athleteIdentity, expires_delta=timedelta(days=30))
         resp = jsonify({"first_name": athlete.firstname, "id": athlete.id, \
                         "success":True if athlete else ""})
 
         set_access_cookies(resp, JWT)
         
-        # resp.set_cookie(
-        #     'JWT',
-        #     JWT,
-        #     httponly=True,
-        #     secure=SECURE,
-        #     samesite='None' if SECURE else 'Lax',
-        #     max_age=60*60*24*30  # 30 days
-        # )
-        # print(SECURE)
         return resp
     #     return resp, 200
     
@@ -79,31 +73,10 @@ def authenticate_me():
         return jsonify({"error": "Failed to authenticate"}), 500
     
 @app.route('/auth/logout', methods=['POST'])
-def logout():
-    # Get refresh token from HttpOnly cookie
-    refresh_token = request.cookies.get('JWT')
-    if not refresh_token:
-        resp = make_response(
-            jsonify({"success": 'no token to begin with (weird)'})
-        )
-        return resp, 200
-    try:
-        # Set the cookie to empty string
-        resp = make_response(
-            jsonify({"success": 'token removed successfully'})
-        )
-        resp.set_cookie(
-            'JWT',
-            '',
-            httponly=True,
-            secure=True,
-            samesite='None',
-            max_age=0       # Forces browser to immediately delete the cookie
-        )
-        return resp, 200
-    except Exception as e:
-        print({'type': 'ERROR', 'message': str(e)})
-        return jsonify({"error": "Failed to logout (an unknown error occurred)"}), 500
+def logout_with_cookies():
+    response = jsonify({"msg": "logout successful"})
+    unset_jwt_cookies(response)
+    return response
     
 @app.route('/auth/whoami', methods=['GET'])
 @jwt_required()
@@ -112,7 +85,7 @@ def who_am_i():
     # TODO: query database to figure out the user's real first name
     # Authenticate user
     resp = make_response(
-            jsonify({"first_name": "FIRST NAME", "id": user, "success":True})
+            jsonify({"first_name": user["first_name"], "id": user["id"], "success":True})
         )
     return resp, 200
 
@@ -121,7 +94,29 @@ def who_am_i():
 ##############
 @app.route('/activities', methods=['GET'])
 def get_activities():
+    before, after = request.args.get('after'), request.args.get('before')
+    if before is None or after is None:
+        return jsonify({"msg":"Missing start or end date in request"}), 400
+    # TODO: cache & fetch activities in database
+    # Get activities from stravalib
+    user = get_jwt_identity()
+
+    # Create client for this request
+    g.client = Client()
+
+    # Fetch activities for this user
+
+    # Return activities
+
     return jsonify({"message": "I am a placeholder!"}), 200
+
+
+    #Number of activities requested in each call 
+    # Better performance could be achieved with smaller pages and more parallel requests
+    # but it's important to be mindful of strava's rate limits. 
+    #Fetching 200 activities typically takes around 2 seconds, an acceptable latency 
+    
+    PER_PAGE = 200;
 
 if __name__ == "__main__":
     app.run(port=3011, debug=True)
